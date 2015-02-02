a6e4568c6ef33b471b28f066c9600673f88f14d0
Nutti <nutti.metro@gmail.com>
2015-02-02 22:30:48 +0900

    Change login process ChildBrowser to InAppBrowser

--- README.md
+++ README.md
@@ -8,3 +8,17 @@ This example uses data and services from http://database.com
 Dependencies:
 * PhoneGap:  http://www.phonegap.com
 * PhoneGap ChildBrowser Plugin: https://github.com/phonegap/phonegap-plugins
+
+Installation:
+
+1. install InAppBrowser
+
+ $ cordova plugin add org.apache.cordova.inappbrowser
+
+2. config.xml
+
+add params showed below.
+
+    <feature name="InAppBrowser">
+      <param name="ios-package" value="CDVInAppBrowser" />
+    </feature>
--- www/js/salesforceWrapper.js
+++ www/js/salesforceWrapper.js
@@ -1,57 +1,60 @@
-
-function SalesforceWrapper() {
+function SalesforceWrapper()
+{
     /* AUTHENTICATION PARAMETERS */
     this.loginUrl = 'https://login.salesforce.com/';
     this.clientId = 'YOUR_KEY_HERE';
     this.redirectUri = 'https://login.salesforce.com/services/oauth2/success';
-    
+
     /* CLASS VARIABLES */
-    this.cb = undefined;     //ChildBrowser in PhoneGap
-    this.client = undefined; //forceTk client instance
-    
+    this.cb = undefined;      // InAppBrowser
+    this.client = undefined;  // forceTk client instance
+
     this.init();
 }
 
 SalesforceWrapper.prototype.init = function() {
     this.client = new forcetk.Client(this.clientId, this.loginUrl);
-    this.cb = window.plugins.childBrowser;
-}
+};
 
 SalesforceWrapper.prototype.login = function (successCallback) {
+    var url = this.getAuthorizeURL(this.loginUrl, this.clientId, this.redirectUri);
+    this.cb = window.open(url, "_blank", 'location=no,toolbar=no,clearsessioncache=yes');
     this.loginSuccess = successCallback;
+
     var self = this;
-    self.cb.onLocationChange = function (loc) {
-        if (loc.search(self.redirectUri) >= 0) {
-            self.cb.close();
-            self.sessionCallback(unescape(loc));
-        }
-    };
-    self.cb.showWebPage(self.getAuthorizeUrl(self.loginUrl, self.clientId, self.redirectUri));
-}
+    self.cb.addEventListener('loadstop', function(e) {
+	    var loc = e.url;
+	    if (loc.indexOf(self.redirectUri) != -1){
+		self.cb.close();
+		self.sessionCallback(unescape(loc));
+	    }
+	});
+};
 
-SalesforceWrapper.prototype.getAuthorizeUrl = function (loginUrl, clientId, redirectUri) {
-    return loginUrl + 'services/oauth2/authorize?display=touch' + '&response_type=token&client_id=' + escape(clientId) + '&redirect_uri=' + escape(redirectUri);
-}
+SalesforceWrapper.prototype.getAuthorizeURL = function (loginUrl, clientId, redirectUri) {
+       return loginUrl + 'services/oauth2/authorize?' + '&response_type=token&client_id=' + encodeURIComponent(clientId) + '&redirect_uri=' + encodeURIComponent(redirectUri);
+};
 
-SalesforceWrapper.prototype.sessionCallback = function(loc) {    var oauthResponse = {};
-    
+SalesforceWrapper.prototype.sessionCallback = function (loc) {
+    var oauthResponse = {};
     var fragment = loc.split("#")[1];
-    
+
     if (fragment) {
-        var nvps = fragment.split('&');
-        for (var nvp in nvps) {
-            var parts = nvps[nvp].split('=');
-            oauthResponse[parts[0]] = unescape(parts[1]);
-        }
+	var nvps = fragment.split('&');
+	for (var nvp in nvps) {
+	    var parts = nvps[nvp].split('=');
+	    oauthResponse[parts[0]] = unescape(parts[1]);
+	}
     }
-    
+
     if (typeof oauthResponse === 'undefined' || typeof oauthResponse['access_token'] === 'undefined') {
-        console.log("error");
-    } else {
-        this.client.setSessionToken(oauthResponse.access_token, null, oauthResponse.instance_url);
-        if ( this.loginSuccess ) {
-            this.loginSuccess();
-        }
+	console.log("error");
+    }
+    else {
+	this.client.setSessionToken(oauthResponse.access_token, null, oauthResponse.instance_url);
+	if (this.loginSuccess){
+	    this.loginSuccess();
+	}
     }
     this.loginSuccess = undefined;
-}
+};
